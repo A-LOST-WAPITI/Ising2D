66d8dc193f9c2a4862b8a3439e9ec64b5f5ca2eb
a-lost-wapiti <a_lost_wapiti@outlook.com>
2020-07-12 20:35:52 +0800

    增加了多次取样求平均的过程（嗨呀一直相加居然这个才是最重要的

--- Main.jl
+++ Main.jl
@@ -72,6 +72,77 @@ function _Findσ(
 end
 
 
+function _MC!(Status::Array{Int64}, n::Int64,  pCluster::Float64, steps::Int64, measure::Bool)
+    σASTemp = zeros(steps + 1)
+    Acorr = zeros(steps + 1)
+    σA::Float64 = 0
+
+    # 时间序列循环
+    for time = 1:steps
+        index = CartesianIndex(rand(1:n), rand(1:n))
+        state = Status[index]
+
+        StatusCheck = falses(n, n)
+
+        CenterIndices = Array{CartesianIndex}([index])
+        Cluster = Array{CartesianIndex}([index]) # 用于存储本次生成的cluster的格点位置的数组
+        StatusCheck[index] = true
+
+        while lastindex(CenterIndices) > 0
+            index = popfirst!(CenterIndices)
+
+            xIndex = index[1]
+            yIndex = index[2]
+            XNei, YNei = _Nei(xIndex, yIndex, n)
+
+            for i in XNei
+                indexTemp = CartesianIndex(i, yIndex)
+                if !StatusCheck[indexTemp] && Status[indexTemp] == state
+                    if rand() < pCluster
+                        push!(CenterIndices, indexTemp)
+                        push!(Cluster, indexTemp)
+                        StatusCheck[indexTemp] = true
+                    end
+                end
+            end
+            for j in YNei
+                indexTemp = CartesianIndex(xIndex, j)
+                if !StatusCheck[indexTemp] && Status[indexTemp] == state
+                    if rand() < pCluster
+                        push!(CenterIndices, indexTemp)
+                        push!(Cluster, indexTemp)
+                        StatusCheck[indexTemp] = true
+                    end
+                end
+            end
+        end
+
+        for index in Cluster
+            Status[index] *= -1
+        end
+
+        if !measure
+            σA = sum(Status)/n^2 |> abs # 求平均后绝对值
+            σASTemp[2:time + 1] .= σASTemp[1:time]
+            σASTemp[1] = σA
+            for j = 1:time + 1
+                Acorr[j] += σA * σASTemp[j]
+            end
+            
+            if Acorr[time + 1] |> iszero
+                break
+            end
+        else
+            σA += sum(Status)/n^2 |> abs # 求平均后绝对值
+        end
+    end
+
+    σA /= steps
+
+    return σA
+end
+
+
 @doc md"""
     _Main(
         nPowMax::Int64 = 5, 
@@ -86,10 +157,11 @@ end
 """ ->
 function _Main(
     nPowMax::Int64 = 6, 
-    timeScale::Int64 = 1000, 
+    timeScale::Int64 = 20000, 
+    measureScale::Int64 = 2000, 
     TMin::Int64 = 1,
     TMax::Int64 = 4,
-    TTick::Float64 = 0.01
+    TTick::Float64 = 0.05
 )
     TS = collect(TMin:TTick:TMax)    # 温度序列
     σAS = similar(TS)  # 存储温度序列对应平均自旋的数组
@@ -101,73 +173,19 @@ function _Main(
         
         # 生成每次实验在不同温度下通用的初始状态
         # RawStatus::Array{Int64} = rand((-1, 1), (n, n))
-        RawStatus = ones(n, n)
+        RawStatus = ones(Int64, (n, n))
         println("\nThe number of nodes is $(n)")
         println("*"^20)
         
         # 对于给定的单方向格点数遍历各个不同的温度
         for (i, T) in enumerate(TS)
             Status = RawStatus  # 赋初值
-            σASTemp = Array{Float64}([])
-            Acorr = Array{Float64}([])
-            σA::Float64 = 0
+            
             pCluster = 1 - exp(-(J₁ + J₂)/T)
 
             # 时间序列循环
-            for time = 1:timeScale
-                index = CartesianIndex(rand(1:n), rand(1:n))
-                state = Status[index]
-
-                StatusCheck = falses(n, n)
-
-                CenterIndices = Array{CartesianIndex}([index])
-                Cluster = Array{CartesianIndex}([index]) # 用于存储本次生成的cluster的格点位置的数组
-                StatusCheck[index] = true
-
-                while lastindex(CenterIndices) > 0
-                    index = popfirst!(CenterIndices)
-
-                    xIndex = index[1]
-                    yIndex = index[2]
-                    XNei, YNei = _Nei(xIndex, yIndex, n)
-
-                    for i in XNei
-                        indexTemp = CartesianIndex(i, yIndex)
-                        if !StatusCheck[indexTemp] && Status[indexTemp] == state
-                            if rand() < pCluster
-                                push!(CenterIndices, indexTemp)
-                                push!(Cluster, indexTemp)
-                                StatusCheck[indexTemp] = true
-                            end
-                        end
-                    end
-                    for j in YNei
-                        indexTemp = CartesianIndex(xIndex, j)
-                        if !StatusCheck[indexTemp] && Status[indexTemp] == state
-                            if rand() < pCluster
-                                push!(CenterIndices, indexTemp)
-                                push!(Cluster, indexTemp)
-                                StatusCheck[indexTemp] = true
-                            end
-                        end
-                    end
-                end
-
-                for index in Cluster
-                    Status[index] *= -1
-                end
-
-                σA = sum(Status)/n^2 |> abs # 求平均后绝对值
-                pushfirst!(σASTemp, σA)
-                push!(Acorr, 0)
-                for j in eachindex(σASTemp)
-                    Acorr[j] += σA * σASTemp[j]
-                end
-                
-                if last(Acorr) |> iszero
-                    break
-                end
-            end
+            _MC!(Status, n, pCluster, timeScale, false)
+            σA = _MC!(Status, n, pCluster, timeScale, true)
 
             println("Temperature\t", T, "\tσAverage\t", σA)
             σAS[i] = σA    # 存储遍历数据
@@ -185,10 +203,12 @@ function _Main(
     title!("σAverage-nodes")  # 图名
     savefig("sigmaAverage-nodes.png")
 
+    #=
     solutionIndex = findfirst(iszero, σAS) # 找到第一个平均自旋降至0的温度
     println("\n", "*"^20)
     println("Solution is:\t", TS[solutionIndex])    # 将解打印
     println()
+    =#
 
     σAST = TS .|> (x -> _Findσ(x, TMax))    # 使用二分法得到的平均场近似理论解存于σAST中
 
